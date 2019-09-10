5e199d1e98f00763f2fd05f13def2567867d9145
roy1210 <rswkure@gmail.com>
2019-09-11 05:42:59 +0800

    Applied SEGA font at title

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ name, id, email }) => {
   return (
     <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img src={`https://robohash.org/${id}200x200' alt='robots`} />
+      <img src={`https://robohash.org/${id}200x200' alt='robots`} alt='card' />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
