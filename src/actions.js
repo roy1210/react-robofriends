52cf854c64628331079e652709a2900045ebbb65
roy1210 <rswkure@gmail.com>
2019-09-12 14:14:23 +0800

    Changed fetch url to `https` instead of `http`

--- src/actions.js
+++ src/actions.js
@@ -18,10 +18,11 @@ export const setSearchField = text => ({
 });
 
 // redux-thunk way. returns function (Higher order function)
+// waiting promise returns
 export const requestRobots = () => dispatch => {
   // no payload due to no arg
   dispatch({ type: REQUEST_ROBOTS_PENDING });
-  fetch('http://jsonplaceholder.typicode.com/users')
+  fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(data => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
     .catch(error => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error }));
