cbb0250a709051cf537e86a4bcd53eb1e579155b
roy1210 <rswkure@gmail.com>
2019-09-14 09:42:31 +0800

    Updates

---
+++ precache-manifest.a9f64aa374ca6b0cedf73624b053ab08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908f5d089e32dab6921e41eaab1c1fc2",
+    "url": "/react-robofriends/index.html"
+  },
+  {
+    "revision": "ff6165ceb8b1765e7f26",
+    "url": "/react-robofriends/static/css/main.375d64bb.chunk.css"
+  },
+  {
+    "revision": "982b23a70daaac44767c",
+    "url": "/react-robofriends/static/js/2.0ba4b5cb.chunk.js"
+  },
+  {
+    "revision": "ff6165ceb8b1765e7f26",
+    "url": "/react-robofriends/static/js/main.32037b86.chunk.js"
+  },
+  {
+    "revision": "58f13911b1e9f497f7e9",
+    "url": "/react-robofriends/static/js/runtime~main.9e4e6ba2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
