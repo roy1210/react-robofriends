1cbe67f70f68c3408d785a5e32466457804d929e
roy1210 <rswkure@gmail.com>
2019-09-14 10:42:34 +0800

    Modified rendering photo size

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({ name, id, email }) => {
   return (
     // dib: responsible with window width
     <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img src={`https://robohash.org/${id}200x200' alt='robots`} alt='card' />
+      <img src={`https://robohash.org/${id}?size=200x200`} alt='robots' />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
